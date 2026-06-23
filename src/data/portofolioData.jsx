89600709fec8462832d0427e539e7254efd2f86c
Andri Pramuji <thedrongs@gmail.com>
2026-06-23 17:19:52 +0700

    fix : centered the mobile modal view

--- src/data/portofolioData.jsx
+++ src/data/portofolioData.jsx
@@ -773,7 +773,7 @@ export const selectedProjects = [
     techStackIconKeys: ["html5", "javascript", "react", "mongodb", "api"],
   },
   {
-    title: "Musa Project",
+    title: "Crewing Offshore Recruitment App",
     period: "Jul 2023 – Dec 2023",
     screenshotFolder: "musaproject",
     screenshotDisplay: "mobile",
